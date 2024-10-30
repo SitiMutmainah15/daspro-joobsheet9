150754b79f27bad6b6997ac48c0dc73311784374
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-10-30 23:19:27 +0700

    perubahan soal 1

--- SearchNilai21.java
+++ SearchNilai21.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class SearchNilai21 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
-        
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMahasiswa];
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang dicari : ");
+        int key = sc.nextInt();
+
+        int indeks = -1;
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (key == nilaiMhs[i]) {
+                indeks = i;
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (indeks + 1));
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
     }
 }
